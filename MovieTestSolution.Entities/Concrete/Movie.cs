73f889f8b96af82f6e89407fe6621d46902a2752
someverything <rustameyvazov@icloud.com>
2025-01-20 21:00:55 +0400

    Add project files.

---
+++ MovieTestSolution.Entities/Concrete/Movie.cs
@@ -0,0 +1,16 @@
+﻿using MovieTestSolution.Entities.Concrete.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTestSolution.Entities.Concrete
+{
+    public class Movie : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
